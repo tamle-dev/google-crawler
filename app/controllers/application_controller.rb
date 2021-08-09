7c0493272e936eca1cdbe6cf19831d35e8733283
Tâm BE <tam.cl@tradingfoe.com>
2021-08-09 14:28:29 +0700

    refactor code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     dashboards_path
   end
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
