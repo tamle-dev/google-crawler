13b1f908177c09a207f64574863aed4602c0c9d6
Tâm BE <tam.cl@tradingfoe.com>
2021-08-10 16:42:01 +0700

    html writer service

---
+++ app/services/keyword_service/html_writer.rb
@@ -0,0 +1,30 @@
+# KeywordService::HtmlWriter.new(keyword, doc).exec
+module KeywordService
+  class HtmlWriter
+    attr_reader :keyword, :doc
+
+    def initialize(keyword, doc)
+      @keyword = keyword
+      @doc     = doc
+    end
+
+    def exec
+      output_file = Tempfile.new(["result_#{keyword.value}_#{keyword.id}", '.html'])
+      output_file.binmode
+      output_file.write doc.to_html
+      output_file.flush
+      
+      attachment = Attachment.new(file: output_file)
+      attachment.save
+
+      update_html_attachment(attachment)
+    end
+
+    private
+
+    def update_html_attachment(attachment)
+      keyword.update(html_attachment_id: attachment.id)
+      keyword
+    end
+  end
+end
