b305e95e004566bed7f04748714ea68d0e4d2e17
Tâm BE <tam.cl@tradingfoe.com>
2021-08-10 15:52:05 +0700

    keyword updater service

---
+++ app/services/keyword_service/updater.rb
@@ -0,0 +1,16 @@
+# KeywordService::Updater.new(resource, params).exec
+module KeywordService
+  class Updater
+    attr_reader :resource, :params
+
+    def initialize(resource, params)
+      @resource = resource
+      @params   = params
+    end
+
+    def exec
+      resource.update(params)
+      resource
+    end
+  end
+end
