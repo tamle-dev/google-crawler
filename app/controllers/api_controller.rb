3c2108ef131d2f21050ffc04b878786c9c43a192
Tâm BE <tam.cl@tradingfoe.com>
2021-08-11 12:11:34 +0700

    paging for API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,4 +3,14 @@ class ApiController < ::ActionController::API
   include ApiResponser
   include ApiParamValidation
   include ApiErrorHandler
+
+  protected
+
+  def paging(collection)
+    collection.page(params[:page]).per(per_page)
+  end
+
+  def per_page
+    params[:per_page] || 10
+  end
 end
